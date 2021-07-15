8ec68fc4151a81f502709dcf9a77184b56fb0e37
futlife-pureLust <futlife.love.daimon@gmail.com>
2021-07-15 21:56:19 +0800

    v2(err)

--- app/src/main/java/com/example/ordermeal/MainActivity.kt
+++ app/src/main/java/com/example/ordermeal/MainActivity.kt
@@ -3,9 +3,33 @@ package com.example.ordermeal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//手動import
+import android.app.Activity
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
+
+//第一步：撰寫MainActivity，按下按鈕，切換至Main2Activtiy
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //「」「」
+      btn_choice.setOnClickListener{
+            //透過Intent切換至Main2Activity，並傳遞requestCode
+            startActivityForResult(Intent(this,Main2Activity::class.java),1)
+        }
+    }
+
+    //第二步：建立onActivityResult()，接收返回資料，將data內容讀出，以textView顯示
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode,resultCode,data)
+        //判斷Bundle是否不為空
+        val b = data?.extras?: return
+        //驗證請求對像與回傳狀態
+        if(requestCode==1&&resultCode==Activity.RESULT_OK){
+            tv_meal.text=String.format("飲料： %s\n\n甜度： %s\n\n冰塊： %s\n\n",b.getString("drink"),b.getString("sugar"),b.getString("ice"))
+        }
     }
 }
+
--- app/src/main/java/com/example/ordermeal/MainActivity2.kt
+++ app/src/main/java/com/example/ordermeal/MainActivity2.kt
@@ -3,9 +3,48 @@ package com.example.ordermeal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//手動import
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
+
 class MainActivity2 : AppCompatActivity() {
+    //切換頁並夾帶資料
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        //接收並顯示呼叫者傳來桌號資料
+        intent?.extras?.let{
+            tv_value2.text  = it.getString("key")
+        }
+
+        //第三步：設定Button監聽事件，判斷是否輸入飲料名稱，並讀取RadioGroup數值
+        btn_send.setOnClickListener{
+            if(ed_drink.length()<1)
+                Toast.makeText(this,"請輸入飲料名稱",Toast.LENGTH_SHORT).show()
+            else{
+                //宣告Bundle
+                val b = Bundle()
+                //取得EditText字串內容，把飲料名稱與甜度及冰塊資訊放入Bundle
+                b.putString("drink",ed_drink.text.toString())
+
+                b.putString(
+                    "sugar",radioGroup1.findViewById<RadioButton>(
+                        radioGroup1.checkedRadioButtonId
+                    )
+                )
+                b.putString(
+                    "ice",radioGroup2.findViewById<RadioButton>(
+                        radioGroup2.checkedRadioButtonId
+                    ).text.toString()
+                )
+                //用Activity.RESULT_OK標記執行狀態並記錄Intent
+                setResult(Activity.RESULT_OK, Intent().putExtra(b))
+                finish()
+            }
+        }
     }
 }
